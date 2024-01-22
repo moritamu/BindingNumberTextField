0b9c6b93dff472e22a75691a0bb2dc455ab4e090
morisum <moris@r.sannet.ne.jp>
2024-01-23 05:29:36 +0900

    完成！

--- BindingNumberTextField/NumberTextField.swift
+++ BindingNumberTextField/NumberTextField.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct NumberTextField: View {
+    var title: String
+    
+    @Binding var number: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(title + ":")
+            TextField("0", text: $number)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numbersAndPunctuation)
+            if numCheck(number) {
+                Image(systemName: "checkmark")
+                    .foregroundColor(.blue)
+            } else {
+                Image(systemName: "arrow.left")
+                    .foregroundColor(.red)
+            }
+            
+        }.frame(width: 150)
+        
+    }
+    func numCheck(_ number: String) -> Bool {
+        guard let num = Int(number) else {
+            return false
+        }
+        return num > 0
     }
 }
 
 #Preview {
-    NumberTextField()
+    NumberTextField(title: "テスト", number: .constant("123"))
 }
