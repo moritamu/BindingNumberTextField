b73a2f8c3acc2afe9c424b065e2f1708fe535bd0
morisum <moris@r.sannet.ne.jp>
2024-01-23 05:28:52 +0900

    とりあえず完成

---
+++ BindingNumberTextField/NumberTextField.swift
@@ -0,0 +1,18 @@
+//
+//  NumberTextField.swift
+//  BindingNumberTextField
+//
+//  Created by MsMacM on 2024/01/22.
+//
+
+import SwiftUI
+
+struct NumberTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NumberTextField()
+}
