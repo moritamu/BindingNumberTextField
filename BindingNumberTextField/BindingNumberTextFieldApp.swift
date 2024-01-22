8acbb73c3cca9210bb599615472e9e316259fde0
morisum <moris@r.sannet.ne.jp>
2024-01-22 17:17:15 +0900

    Initial Commit

---
+++ BindingNumberTextField/BindingNumberTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingNumberTextFieldApp.swift
+//  BindingNumberTextField
+//
+//  Created by MsMacM on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BindingNumberTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
